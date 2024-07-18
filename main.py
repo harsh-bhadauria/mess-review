9032338b944aa5464a31d9fbc0839e168f9b643e
Rxven0us <hsb.harsh070@gmail.com>
2024-07-18 12:27:34 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello World!")
+st.header("Let's learn about ML")
+st.subheader("Ravens are cool")
+st.info("So are penguins!!!")
+st.warning("And cats too")
+st.error("Wrong password pookie")
+st.success("Who's a good boy?")
